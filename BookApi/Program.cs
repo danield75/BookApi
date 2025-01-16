c65d5b79f560511616407413111689ede8a2a066
Daniel Drion <daniel.drion@telenet.be>
2025-01-16 15:21:13 +0100

    Configure CORS

--- BookApi/Program.cs
+++ BookApi/Program.cs
@@ -8,8 +8,19 @@ namespace BookApi
 
             // add services
             builder.Services.AddControllers();
-
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyCors", builder =>
+                {
+                    builder
+                        .WithOrigins("http://localhost:4200")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
             var app = builder.Build();
+
+            app.UseCors("MyCors");
 
             // add mapping
             app.MapControllers();
